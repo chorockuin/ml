bfd5a9af904cc532044c598f6875bc57e1110f38
root <chorockuin@gmail.com>
2022-06-03 14:51:16 +0000

    add dropout, early_stopping

---
+++ neural_network_early_stopping.py
@@ -0,0 +1,59 @@
+from multiprocessing.dummy import active_children
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+
+(x, y), _ = mnist.load_data()
+print(x.shape)
+x = x / 255.0
+
+data = tf.data.Dataset.from_tensor_slices((x, y))
+data_num = len(data)
+
+train_data_num = data_num * 35 // 100
+valid_data_num = data_num * 15 // 100
+test_data_num = data_num - (train_data_num + valid_data_num)
+print(data_num, train_data_num, valid_data_num, test_data_num)
+
+train_data = data.take(train_data_num).shuffle(1024, reshuffle_each_iteration=True).batch(32)
+valid_data = data.skip(train_data_num).take(valid_data_num).batch(128)
+test_data = data.skip(train_data_num + valid_data_num).batch(128)
+print(len(train_data), len(valid_data), len(test_data))
+
+class Model(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(32, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(64, activation='relu')
+        self.dense3 = tf.keras.layers.Dense(128, activation='relu')
+        self.dense4 = tf.keras.layers.Dense(10, activation='softmax')
+
+    def call(self, x):
+        return self.dense4(self.dense3(self.dense2(self.dense1(self.flatten(x)))))
+
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0, patience=3, restore_best_weights=True)
+
+def loss_func(y, y_pred):
+    return tf.losses.sparse_categorical_crossentropy(y, y_pred)
+
+@tf.function
+def train_step(model, x, y, optimizer, train_loss, train_accuracy):
+    with tf.GradientTape() as t:
+        y_pred = model(x)
+        loss = loss_func(y, y_pred)
+    grads = t.gradient(loss, model.trainable_weights)
+    optimizer.apply_gradients(zip(grads, model.trainable_weights))
+    train_loss(loss)
+    train_accuracy(y, y_pred)
+
+@tf.function
+def valid_step(model, x, y, valid_loss, valid_accuracy):
+    y_pred = model(x)
+    loss = loss_func(y, y_pred)
+    valid_loss(loss)
+    valid_accuracy(y, y_pred)
+
+model = Model()
+
+model.compile(optimizer=tf.optimizers.Adam(), loss='sparse_categorical_crossentropy')
+model.fit(train_data, epochs=100, validation_data=valid_data, callbacks=[callback])
